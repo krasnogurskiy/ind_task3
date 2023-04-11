f1c16f2856e05c9ae50bcda652e86536c87a0bd6
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-04-11 10:55:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace card_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
