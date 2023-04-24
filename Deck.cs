0bf19eb79f22a508303303d9b9280cc9e3ed1553
Olesia Baidala <olesya.baidala32@gmail.com>
2023-04-25 00:39:15 +0300

    add constructor for Deck

--- Deck.cs
+++ Deck.cs
@@ -4,22 +4,36 @@ using System.Text;
 
 namespace card_game
 {
-
     class Deck
     {
-        private int players; // Кількість гравців
-        
-        private Stack_of_cards cards;
-        public Deck()
+        private int number_of_players; // Кількість гравців
+        private Card[] cards;
+        public Deck(int _players)
         {
-            LinkedList<Card> cards_in_deck = new LinkedList<Card>();
-            for (uint i = 0; i < 12; ++i)
+            number_of_players = _players;
+            if (_players < 6)
             {
-                for (int j = 0; j < 4; ++j)
-                {
-                    Card c = new Card(i, (Suit)Enum.Parse(typeof(Suit), j.ToString()));
-                    cards_in_deck.AddLast(c);
-                }
+                cards = new[] {new Card(6, Suit.Heart), new Card(7, Suit.Heart), new Card(8, Suit.Heart), new Card(9, Suit.Heart),
+                    new Card(10, Suit.Heart), new Card(11, Suit.Heart), new Card(12, Suit.Heart), new Card(13, Suit.Heart), new Card(1, Suit.Heart),
+                    new Card(6, Suit.Club), new Card(7, Suit.Club), new Card(8, Suit.Club), new Card(9, Suit.Club), new Card(10, Suit.Club),
+                    new Card(11, Suit.Club), new Card(12, Suit.Club), new Card(13, Suit.Club), new Card(1, Suit.Club), new Card(6, Suit.Diamond), new Card(7, Suit.Diamond),
+                    new Card(8, Suit.Diamond), new Card(9, Suit.Diamond), new Card(10, Suit.Diamond), new Card(11, Suit.Diamond),
+                    new Card(12, Suit.Diamond), new Card(13, Suit.Diamond), new Card(1, Suit.Diamond), new Card(6, Suit.Spade), new Card(7, Suit.Spade),
+                    new Card(8, Suit.Spade), new Card(9, Suit.Spade), new Card(10, Suit.Spade), new Card(11, Suit.Spade),
+                    new Card(12, Suit.Spade), new Card(13, Suit.Spade), new Card(1, Suit.Spade) };
+            }
+            else
+            {
+                cards = new[] {new Card(2, Suit.Heart), new Card(3, Suit.Heart), new Card(4, Suit.Heart), new Card(5, Suit.Heart),
+                    new Card(6, Suit.Heart), new Card(7, Suit.Heart), new Card(8, Suit.Heart), new Card(9, Suit.Heart), new Card(10, Suit.Heart),
+                    new Card(11, Suit.Heart), new Card(12, Suit.Heart), new Card(13, Suit.Heart), new Card(1, Suit.Heart), new Card(2, Suit.Club), new Card(3, Suit.Club),
+                    new Card(4, Suit.Club), new Card(5, Suit.Club), new Card(6, Suit.Club), new Card(7, Suit.Club), new Card(8, Suit.Club),
+                    new Card(9, Suit.Club), new Card(10, Suit.Club), new Card(11, Suit.Club), new Card(12, Suit.Club), new Card(13, Suit.Club), new Card(1, Suit.Club),
+                    new Card(2, Suit.Diamond), new Card(3, Suit.Diamond), new Card(4, Suit.Diamond), new Card(5, Suit.Diamond), new Card(6, Suit.Diamond),
+                    new Card(7, Suit.Diamond), new Card(8, Suit.Diamond), new Card(9, Suit.Diamond), new Card(10, Suit.Diamond), new Card(11, Suit.Diamond),
+                    new Card(12, Suit.Diamond), new Card(13, Suit.Diamond), new Card(1, Suit.Diamond), new Card(2, Suit.Spade), new Card(3, Suit.Spade), new Card(4, Suit.Spade),
+                    new Card(5, Suit.Spade), new Card(6, Suit.Spade), new Card(7, Suit.Spade), new Card(8, Suit.Spade), new Card(9, Suit.Spade),
+                    new Card(10, Suit.Spade), new Card(11, Suit.Spade), new Card(12, Suit.Spade), new Card(13, Suit.Spade), new Card(1, Suit.Spade) };
             }
         }
     }
