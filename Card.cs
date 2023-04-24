6b88cffb95b2f48c628b8a51253d0995be050f5a
TruePelmen <oleksiisenyshyn@lnu.edu.ua>
2023-04-24 17:06:06 +0300

    Made Card class

---
+++ Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace card_game
+{
+    // Я шось так поняв, шо нам масть, напевно, не треба буде, ну але я вже написав, тому най буде)))
+    
+
+    //Масть
+    public enum Suit
+    {
+        Heart, 
+        Diamond, 
+        Spade,
+        Club
+    }
+    class Card
+    {
+        private int cost; // Вартість карти
+        public int GetCost() // Геттер
+        {
+            return cost;
+        }
+        public Card(int _cost) { cost = _cost; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,3 +11,4 @@ namespace card_game
         }
     }
 }
+
